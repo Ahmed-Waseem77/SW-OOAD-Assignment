061d4f5646391884b5964e9bb52ad1136f29d620
Mahmoudd29 <mahmoud.abdelhalim03@eng-st.cu.edu.eg>
2024-07-25 21:22:13 +0300

    DbUtils

--- DbUtils/DbUtils.cpp
+++ DbUtils/DbUtils.cpp
@@ -4,12 +4,15 @@ using namespace std;
 DbUtils::DbUtils() {
     // Constructor: checks the connection with the database and returns true if the connection is successful otherwise false.
     try{
-        //instance instance{}; // Creating a single and only connection of the db
-        //client client{mongocxx::uri{}};
+       if(!databaseConnection) {
+            throw runtime_error ("Database connection failed");
+        }
         cout << "Connected to the database" << endl;
-    } catch (const exception& e) {
+    } 
+    catch (const exception& e) {
         cout << "Error: " << e.what() << endl;
     }
+
     
 }
 
@@ -18,23 +21,81 @@ DbUtils::DbUtils() {
 bool DbUtils::createUserRecord(const string& collection, const string& jsonString) {
     
     try{
-        
-
+        if(!connectionFlag) {
+            throw runtime_error ("Database connection failed");
+        }
+        //Insert the json string into the collection using the database connection pointer
+        cout << "User record created successfully" << endl;
     }
-
+    catch (const exception& e) {
+            cout << "Error: " << e.what() << endl;
+        }
 
 
     
 }
 
 bool DbUtils::updateUserRecord(const string& collection, const string& documentID, const string& jsonString) {
+    try{
+        if(!connectionFlag) {
+            throw runtime_error ("Database connection failed");
+        }
+        
+        //update the json string in the collection using the database connection pointer and the documentID
+        cout << "User record updated successfully" << endl;
+    }
+    catch (const exception& e) {
+            cout << "Error: " << e.what() << endl;
+        }
     
 }
 
 bool DbUtils::removeUserRecord(const string& collection, const string& documentID) {
+    try{
+        if(!connectionFlag) {
+            throw runtime_error ("Database connection failed");
+        }
+        //remove the json string in the collection using the database connection pointer and the documentID
+        cout << "User record removed successfully" << endl;
+    }
+    catch (const exception& e) {
+            cout << "Error: " << e.what() << endl;
+        }
    
 }
 
-int DbUtils::countDocuments(const string& collection) const {
-    
+int DbUtils::countDocuments(const std::string &collection)
+{
+     try{
+        if(!connectionFlag) {
+            throw runtime_error ("Database connection failed");
+        }
+        //count the number of documents in the collection using the database connection pointer
+        cout << "Number of documents in the collection: " << collection << " is 5" << endl;
+    }
+    catch (const exception& e) {
+            cout << "Error: " << e.what() << endl;
+        }
 }
+
+DbUtils *DbUtils::getInstance()
+{
+    if(databaseInstance == nullptr)
+    {
+        databaseInstance = new DbUtils();
+        thread connectionCheckThread(&DbUtils::checkDatabaseConnectionAsynchronously, databaseInstance);
+        connectionCheckThread.detach();
+    }
+    return databaseInstance;
+}
+
+
+bool DbUtils::checkDatabaseConnectionAsynchronously()
+{
+    while(databaseConnection != nullptr)
+    {
+        connectionFlag = true;  
+    }
+    connectionFlag = false;
+}
+
--- DbUtils/DbUtils.hpp
+++ DbUtils/DbUtils.hpp
@@ -2,6 +2,7 @@
 #define DBUTILS_HPP
 #include <../include/include.hpp>
 #include "defines.hpp"
+#include <thread>
  //Singlton Class since it is only one databaseConnection of the class
 class DbUtils {
 private:
@@ -10,12 +11,20 @@ private:
     static connection* databaseConnection;
 
 public:
+    static bool connectionFlag;
+    
     DbUtils(const DbUtils& obj) = delete;
 
-    bool createUserRecord(const std::string& collection, const std::string& jsonString);
-    bool updateUserRecord(const std::string& collection, const std::string& documentID, const std::string& jsonString);
-    bool removeUserRecord(const std::string& collection, const std::string& documentID);
-    int countDocuments(const std::string& collection);
+    static bool createUserRecord(const std::string& collection, const std::string& jsonString);
+    static bool updateUserRecord(const std::string& collection, const std::string& documentID, const std::string& jsonString);
+    static bool removeUserRecord(const std::string& collection, const std::string& documentID);
+    static int countDocuments(const std::string& collection);
+
+    static DbUtils* getInstance();
+    static void closeConnection();
+    static bool isConnected();
+    static bool checkDatabaseConnectionAsynchronously();
+
 };
 
 #endif // DBUTILS_H
